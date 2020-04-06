eacfebc1ef32bf386cb48fac7e8ab20155471f56
neer17 <neerajsewanisrt8@gmail.com>
2020-04-06 15:46:55 +0530

    fix: panel.js cross button, image alignment

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,35 +1,105 @@
 import React, { Component } from "react"
-import {FaImage} from "react-icons/fa"
-import {IconContext} from 'react-icons'
+import { FaRegFilePdf, FaImage } from "react-icons/fa"
+import { TiDocumentText } from "react-icons/ti"
+import { IconContext } from "react-icons"
 
 import { MyContext } from "./Provider"
+import { IMAGE_SIZE } from "./../utils/constants"
 
+// rendered in home.js
+//  controlled from home-form.js
 class Panel extends Component {
+  constructor(props) {
+    super(props)
+    this.ref = React.createRef()
+  }
 
-    findFileType = fileType => {
-        switch(fileType) {
-            case 'image/jpeg':
-            case 'image/jpg': return "image"
-            case 'application/pdf': return "pdf"
-            default: return "file"
-        }
+  componentDidMount() {
+    document.addEventListener("mousedown", this.hidePanel)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.hidePanel)
+  }
+
+  hidePanel = event => {
+    if (this.ref && !this.ref.current.contains(event.target))
+      this.context.updateState({
+        showMoreFilesPanel: false
+      })
+  }
+
+  onCancel = fileName => {
+    this.context.cancel(fileName)
+  }
+
+  closePanel = () => {
+    this.context.updateState({
+      showMoreFilesPanel: false
+    })
+  }
+
+  findFileType = fileType => {
+    switch (fileType) {
+      case "image/jpeg":
+      case "image/jpg":
+        return (
+          <IconContext.Provider
+            value={{
+              color: "blue",
+              className: "global-class-name",
+              size: IMAGE_SIZE
+            }}
+          >
+            <FaImage />
+          </IconContext.Provider>
+        )
+      case "application/pdf":
+        return (
+          <IconContext.Provider
+            value={{
+              color: "blue",
+              className: "global-class-name",
+              size: IMAGE_SIZE
+            }}
+          >
+            <FaRegFilePdf />
+          </IconContext.Provider>
+        )        
+      default:
+        return (
+          <IconContext.Provider
+            value={{
+              color: "blue",
+              className: "global-class-name",
+              size: IMAGE_SIZE
+            }}
+          >
+            <TiDocumentText />
+          </IconContext.Provider>
+        )  
     }
+  }
 
   render() {
     const files = this.context.getState().files
 
     return (
-      <div className="panel container">
+      <div className="panel container " ref={this.ref}>
+        <div className="panel__cross-btn" onClick={this.closePanel}>&times;</div>
+
         {files.length > 0 ? (
           <div>
             {files.map(file => {
               return (
                 <div className="panel__container d-flex p-2" key={file.name}>
-                  <div className="panel__img p-1 mr-4">
-                  {}
+                  <div className="panel__img align-self-center p-1 mr-4">
+                    {this.findFileType(file.type)}
                   </div>
-                  <div className="panel__filename flex-grow-1 p-1">{file.name}</div>
-                  <div className="panel__close-button p-1">&times;</div>
+                  <div className="panel__filename flex-grow-1 p-1">
+                    {file.name}
+                  </div>
+                  <div className="panel__close-button p-1" onClick={this.onCancel.bind(this, file.name)}>&times;</div>
                 </div>
               )
             })}
--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,70 +1,94 @@
 /*
-* This would contain global variables
-* that can be changed from anywhere from the app using the "Context"
-* */
-import React, {Component} from 'react'
+ * This would contain global variables
+ * that can be changed from anywhere from the app using the "Context"
+ * */
+import React, { Component } from "react"
+
+import { reactLocalStorage } from "reactjs-localstorage"
 
 const Context = React.createContext()
 
-Context.displayName = 'ReactContext1'
+Context.displayName = "ReactContext1"
 
 export class Provider extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
+    console.info("constructor")
 
-        this.state = {
-            componentToRender: 'HomeForm',
-            uploadEvent: null,
-            data: null,
-            isAuthenticated: false,
-            showMoreFilesPanel: false,
-            files: []
-        }
+    this.state = {
+      componentToRender: "HomeForm",
+      uploadEvent: null,
+      data: null,
+      isAuthenticated: false,
+      showMoreFilesPanel: false,
+      files: []
     }
+  }
 
+  componentDidMount() {
+    console.info("componentDidMount")
 
-    updateState = (object = {}) => {
-        this.setState({
-            ...object
-        }, () => {
-            console.info('provider.js state ==> ', this.state)
-        })
-    }
+    window.addEventListener('beforeunload', this.cleanUpCode)
+  }
 
-    getState = () => {
-        return this.state
-    }
-
-    cancel = nameOfFile => {
-        //  getting the name of the file and then removing it from the values and finally
-        //  updating the state with new "values"
-        const files = this.state.files
-    
-        files.forEach((file, index) => {
-          if (file.name === nameOfFile) {
-            return files.splice(index, 1)
-          }
-        })
-    
-        this.setState({
-          files
-        })
+  updateState = (object = {}) => {
+    this.setState(
+      {
+        ...object
+      },
+      () => {
+        console.info("provider.js state ==> ", this.state)
       }
+    )
+  }
 
-    render() {
-        return (
-            //  state will now be available to all the components
-            //  as this Provider class would be wrapped around all the components in App.js
-            //  updating and getting the state with respective methods
-            <Context.Provider value={{
-                updateState: this.updateState,
-                getState: this.getState,
-                cancel: this.cancel
-            }}>
-                {this.props.children}
-            </Context.Provider>
-        )
-    }
+  getState = () => {
+    return this.state
+  }
+
+  //  getting the name of the file and then removing it from the values and finally
+  //  updating the state with new "values"
+  cancel = nameOfFile => {
+    const files = this.state.files
+
+    files.forEach((file, index) => {
+      if (file.name === nameOfFile) {
+        return files.splice(index, 1)
+      }
+    })
+
+    this.setState({
+      files
+    })
+  }
+
+  cleanUpCode = () => {
+      reactLocalStorage.setObject('state', this.state)
+  }
+
+  render() {
+    console.info("render")
+
+    return (
+      //  state will now be available to all the components
+      //  as this Provider class would be wrapped around all the components in App.js
+      //  updating and getting the state with respective methods
+      <Context.Provider
+        value={{
+          updateState: this.updateState,
+          getState: this.getState,
+          cancel: this.cancel
+        }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    )
+  }
+
+  componentWillUnmount() {
+    console.info("componentWillUnmount")
+    window.removeEventListener('beforeunload', this.cleanUpCode)
+  }
 }
 
 //  wrap "Consumer" around the stuff in return in any file to get the items passed in "value" above
