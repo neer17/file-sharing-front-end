7c197ac764924a3fd407aa305d0a2ed96500d401
neer17 <neerajsewanisrt8@gmail.com>
2020-04-24 18:21:21 +0530

    fix: condition added.

--- src/utils/betterNumber.js
+++ src/utils/betterNumber.js
@@ -1,35 +1,32 @@
 /* eslint-disable */
 
-
 export const betterNumber = (fileInBytes, inFormat = false) => {
-    const kb = 1024
-    const mb = kb * kb
-    const gb = mb * mb
-    let fileFormat = ''
-    let betterFormat = 0
+  const kb = 1024
+  const mb = kb * kb
+  const gb = mb * mb
+  let fileFormat = ""
+  let betterFormat = 0
 
-    if (fileInBytes < kb) {
-        if (inFormat)
-            return fileFormat = String(Math.round(fileInBytes / kb)).concat('KB')
-        else
-            return betterFormat = Math.round(fileInBytes / kb)
-    }
-    if (fileInBytes > kb) {
-        if (inFormat)
-            return fileFormat = String(Math.round(fileInBytes / kb)).concat('KB')
-        else
-            return betterFormat = Math.round(fileInBytes / kb)
-    }
-    if (fileInBytes > mb) {
-        if (inFormat)
-            return fileFormat = String(Math.round(fileInBytes / mb)).concat('MB')
-        else
-            return betterFormat = Math.round(fileInBytes / mb)
-    }
-    if (fileInBytes > gb) {
-        if (inFormat)
-            return fileFormat = String(Math.round(fileInBytes / gb)).concat('GB')
-        else
-            return betterFormat = Math.round(fileInBytes / gb)
-    }
+  if (fileInBytes === 0) return 0
+
+  if (fileInBytes < kb) {
+    if (inFormat)
+      return (fileFormat = String(Math.round(fileInBytes / kb)).concat("KB"))
+    else return (betterFormat = Math.round(fileInBytes / kb))
+  }
+  if (fileInBytes > kb) {
+    if (inFormat)
+      return (fileFormat = String(Math.round(fileInBytes / kb)).concat("KB"))
+    else return (betterFormat = Math.round(fileInBytes / kb))
+  }
+  if (fileInBytes > mb) {
+    if (inFormat)
+      return (fileFormat = String(Math.round(fileInBytes / mb)).concat("MB"))
+    else return (betterFormat = Math.round(fileInBytes / mb))
+  }
+  if (fileInBytes > gb) {
+    if (inFormat)
+      return (fileFormat = String(Math.round(fileInBytes / gb)).concat("GB"))
+    else return (betterFormat = Math.round(fileInBytes / gb))
+  }
 }
