7b0be74251c54691b1e131c7ac48b44f42974fe4
neer17 <neerajsewanisrt8@gmail.com>
2021-04-06 19:14:05 +0530

    chore: put backend url in .env

--- src/utils/domainConfig.js
+++ src/utils/domainConfig.js
@@ -2,5 +2,5 @@ const production = process.env.REACT_APP_PRODUCTION
 
 //  URL of backend
 // console.info('upload.js production: ', process.env.REACT_APP_PRODUCTION, 'type: ', typeof production)
-const url = production === 'true' ? 'https://file-sharing-app-back-end.herokuapp.com' : 'http://localhost:3003'
+const url = production === 'true' ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:3003'
 export {url}
